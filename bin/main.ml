058f048ddece6c7c79254bfbf65dca450a016312
Felix <felix.wenksy@gmail.com>
2025-11-27 04:48:54 +0100

    fix: lowercase docker image name in deploy script

--- bin/main.ml
+++ bin/main.ml
@@ -4,9 +4,14 @@ let () =
   let db_url =
     Sys.getenv_opt "DATABASE_URL" |> Option.value ~default:"sqlite3:asoiafdle.db"
   in
+  let secret =
+    Sys.getenv_opt "SESSION_SECRET"
+    |> Option.value ~default:"change_me_in_production_please"
+  in
   Lwt_main.run (Db.init_db db_url);
   Dream.run ~interface:"0.0.0.0" ~port:8080
   @@ Dream.logger
+  @@ Dream.set_secret secret
   @@ Dream.sql_pool db_url
   @@ Dream.cookie_sessions ~lifetime:1800.0
   @@ Dream.router
